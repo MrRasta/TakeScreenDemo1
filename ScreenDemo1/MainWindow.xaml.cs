1e9981c1038469ba7da9c1513c969fb1551582d4
MrRasta <germanovpavel@gmail.com>
2015-09-16 20:54:14 +0300

    Added ability to save main monitor content as image

--- ScreenDemo1/MainWindow.xaml.cs
+++ ScreenDemo1/MainWindow.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+using Size = System.Drawing.Size;
 
 namespace ScreenDemo1
 {
@@ -20,6 +17,8 @@ namespace ScreenDemo1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Image _currentImage;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +32,57 @@ namespace ScreenDemo1
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void BtnTakeScreen_Click(object sender, RoutedEventArgs e)
+        {
+            Image allScreen = CaptureScreen(0, 0, SystemParameters.PrimaryScreenWidth, SystemParameters.PrimaryScreenHeight);
+            Img.Source = ConvertImg(allScreen);
+
+            _currentImage = allScreen;
+            BtnSave.IsEnabled = true;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private Image CaptureScreen(double x, double y, double width, double height)
+        {
+            int ix, iy, iw, ih;
+            ix = Convert.ToInt32(x);
+            iy = Convert.ToInt32(y);
+            iw = Convert.ToInt32(width);
+            ih = Convert.ToInt32(height);
+            Image image = new Bitmap(iw, ih, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(image);
+            g.CopyFromScreen(ix, iy, ix, iy, new Size(iw, ih), CopyPixelOperation.SourceCopy);
+
+            return image;
+        }
+
+        private void Save()
+        {
+            SaveFileDialog dlg = new SaveFileDialog { DefaultExt = "png", Filter = "Png Files|*.png" };
+            bool? res = dlg.ShowDialog();
+
+            if (res == true)
+                _currentImage.Save(dlg.FileName, ImageFormat.Png);
+            
+        }
+
+        private static ImageSource ConvertImg(object value)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((Bitmap)value).Save(ms, ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            return image;
         }
     }
 }
